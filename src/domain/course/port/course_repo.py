39a36c78464a7f3861b506c3c3755b6ae06558a3
rdmorozkin <rdmorozkin@domclick.ru>
2024-06-28 08:24:05 +0600

    * add user-repo

---
+++ src/domain/course/port/course_repo.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+from pydantic import UUID4
+
+from src.domain.common.enum.order import Order
+from src.domain.course.dto.course import CourseInCreate, CourseInUpdate
+from src.domain.course.entity.course import Course
+
+
+class ICourseRepo(ABC):
+    
+    @abstractmethod
+    async def create(self, data: CourseInCreate) -> Course: ...
+
+    @abstractmethod
+    async def update(self, data: CourseInUpdate) -> Course: ...
+
+    @abstractmethod
+    async def read_one(self, id_: UUID4) -> Course: ...
+
+    @abstractmethod
+    async def delete(self, id_: UUID4) -> Course: ...
+
+    @abstractmethod
+    async def read_many(self, limit: int, offset: int, order: str, order_by: Order) -> list[Course]: ...
+
+    @abstractmethod
+    async def count(self) -> int: ...
