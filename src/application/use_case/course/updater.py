b25c441dbd8827ad26f05e57da5b6c6bed658692
moneymaker <romasa464@gmail.com>
2024-06-29 15:08:25 +0600

    * refactoring course-updater

--- src/application/use_case/course/updater.py
+++ src/application/use_case/course/updater.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
 
-from src.domain.course.dto.course import CourseInUpdateForUpdater
+from src.domain.course.dto.course import CourseInUpdateForUpdater, CourseInUpdate
 from src.domain.course.entity.course import Course
 from src.domain.course.exception.course.update import CourseUpdateError
 from src.domain.course.port.course_repo import ICourseRepo
+from src.domain.user.enum.roles import UserRoleEnum
 
 
 class ICourseUpdaterCase(ABC):
@@ -18,6 +19,12 @@ class CourseUpdaterCase(ICourseUpdaterCase):
 
     async def update(self, course_in_update: CourseInUpdateForUpdater) -> Course:
         course = await self.__course_repo.read_one(course_in_update.id)
-        if course.author_id != course_in_update.requested_user.id:
+        update_data = CourseInUpdate(**course_in_update.model_dump())
+
+        if (
+            course.author_id != course_in_update.requested_user.id
+            and UserRoleEnum.ADMIN not in course_in_update.requested_user.roles
+        ):
             raise CourseUpdateError("Допускается редактировать только курсы созданные автором")
-        return await self.__course_repo.update(course_in_update)
+
+        return await self.__course_repo.update(update_data)
